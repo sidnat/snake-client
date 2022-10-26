6ba356aea503049695ec34d63bba6d21ccf0e040
Sid Natarajan <sid.nat@gmail.com>
2022-10-26 20:19:26 +0000

    added macro messages

--- input.js
+++ input.js
@@ -10,22 +10,36 @@ const setupInput = (conn) => {
   return stdin;
 };
 
-const handleUserInput = (key) => {
-  const moveSnake = (direction) =>{
-    connection.write(`Move: ${direction}`);
-  }
+let id = -1;
+
+const handleUserInput = (key, speedMultiplier) => {
+  const moveSnake = (direction) => {
+    clearInterval(id);
+
+    id = setInterval(() => {
+      connection.write(`Move: ${direction}`);
+    }, 100 * speedMultiplier);
+
+    id;
+  };
 
   if (key === '\u0003') {
     process.exit();
-  } else if (key === 'w') {      
-    moveSnake('up');
+  } else if (key === 'w') {
+    moveSnake('up', 1);
   } else if (key === 's') {
-    moveSnake('down');
+    moveSnake('down', 1);
   } else if (key === 'a') {
-    moveSnake('left');
+    moveSnake('left', 2);
   } else if (key === 'd') {
-    moveSnake('right');
-  } 
+    moveSnake('right', 2);
+  } else if (key === 't') {
+    connection.write('winner!');
+  } else if (key === 'y') {
+    connection.write('got ya!');
+  } else if (key === 'u') {
+    connection.write('speeeed');
+  }
 };
 
 module.exports = {
