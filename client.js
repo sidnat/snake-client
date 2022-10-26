2252cd02aad12edda3402c1f0809ded5e55baa50
Sid Natarajan <sid.nat@gmail.com>
2022-10-26 17:28:03 +0000

    added event listeners

--- client.js
+++ client.js
@@ -1,13 +1,27 @@
 const net = require("net");
 
 const connect = () => {
+  console.log("Connecting ...");
+
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541,
   });
-
+  
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  
+  conn.on("connect", () => {
+    conn.write("Name: SID");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says again: ", data);
+  });
+  
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,9 +1,5 @@
 const { connect } = require('./client');
-const snakeGame = connect();
+
+connect();
 // establishes a connection with the game server
 
-console.log("Connecting ...");
-
-snakeGame.on("data", (data) => {
-  console.log("Server says: ", data);
-});
