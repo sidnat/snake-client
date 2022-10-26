227a55521e3eb267a452821c17d65ada36a63b72
Sid Natarajan <sid.nat@gmail.com>
2022-10-26 18:28:23 +0000

    created input file

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const { connect } = require('./client');
-const snakeGame = connect();
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { setupInput } = require('./input');
 
 connect();
 setupInput();
